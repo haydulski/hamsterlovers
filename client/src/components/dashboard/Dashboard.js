74f622e42f224e3fcee246e1176767ef0949433c
Damian <dhfxblog@gmail.com>
2020-06-09 18:17:46 +0200

    dashboard begin

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,17 +2,18 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getProfile } from '../../redux/actions/profileAction';
+import Spinner from '../../components/layout/Spinner';
 
 
 const Dashboard = ({ auth, profile, getProfile }) => {
     useEffect(() => {
         getProfile()
     }, [])
-    return (
+    return profile.loading && profile.profile === null ? <Spinner /> :
         <>
-            Dashboard
+            <h1 className='large text-primary'>Dashboard</h1>
+            <p className="lead"><i className="fas fa-user"></i>Welcome {auth.user && auth.user.user.name}</p>
         </>
-    );
 }
 Dashboard.propTypes = {
     auth: PropTypes.object.isRequired,
