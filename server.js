699391af1928a1567ce0e2438aede31e1a678945
Damian <dhfxblog@gmail.com>
2020-06-12 13:45:02 +0200

    prepare to deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const path = require('path');
 
 //polaczenie z MongoAtlas
 connectDB();
@@ -9,15 +10,25 @@ connectDB();
 app.use(express.json({ extended: false }))
 
 //nasluch portu
-app.get('/', (req, res) => {
-    res.send('strona działa');
-})
+// app.get('/', (req, res) => {
+//     res.send('strona działa');
+// })
 
 //routing
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 app.listen(PORT, () => {
