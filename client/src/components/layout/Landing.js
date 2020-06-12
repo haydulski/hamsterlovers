86e8ebef38d860f83561249a1b29b7dff6ff457e
Damian <dhfxblog@gmail.com>
2020-06-12 14:22:56 +0200

    changed landing backgorund

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -10,7 +10,7 @@ const Landing = ({ isAuth }) => {
         <section className="landing">
             <div className="dark-overlay">
                 <div className="landing-inner">
-                    <h1 className="x-large">Developer Connector</h1>
+                    <h1 className="x-large">Developer Place</h1>
                     <p className="lead">
                         Create a developer profile/portfolio, share posts and get help from
                         other developers
